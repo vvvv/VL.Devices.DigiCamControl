796ad70ddc20517612cfaa6a2751e971e63eb8a2
joreg <joreg@vvvv.org>
2022-09-01 11:21:09 +0200

    fix for JpgDecompressor. adds to #1

--- src/JpgDecompressor.cs
+++ src/JpgDecompressor.cs
@@ -2,19 +2,18 @@
 using System.Drawing.Imaging;
 using System.Drawing;
 using System.Runtime.InteropServices;
+using System;
 
 namespace DigiCamControl
 {
     public static class JpgDecompressor
     {
-        public static byte[] DecompressJpeg(byte[] photoBytes)
+        public static byte[] DecompressJpeg(ArraySegment<byte> jpgEncodedBytes)
         {
-            byte[] result = photoBytes;
-            if (photoBytes.Length > 4)
+            if (jpgEncodedBytes.Count > 4)
             {
-                using (MemoryStream inStream = new MemoryStream(photoBytes))
+                using (MemoryStream inStream = new MemoryStream(jpgEncodedBytes.Array))
                 {
-                    //using(var img = Image.FromStream(inStream))
                     using (Bitmap bmp = new Bitmap(inStream))
                     {
                         var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
@@ -22,19 +21,21 @@ namespace DigiCamControl
                         var data = bmp.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
 
                         var arraySize = data.Stride * data.Height;
-                        result = new byte[arraySize];
+                        var result = new byte[arraySize];
 
-                        var lineSize = data.Width * 4;
+                        //var lineSize = data.Width * 4;
                         //for (int i = 0; i < data.Height; i++)
                         {
                             Marshal.Copy(data.Scan0, result, 0, arraySize);
                         }
 
                         bmp.UnlockBits(data);
+
+                        return result;
                     }
                 } 
             }
-            return result;
+            return new byte[0];
         }
     }
 } 
